8b9d534fe04d16f39f87605b7afb280fdb6f8583
MKRTCHIAN <mkrtchia@polytech.unice.fr>
2011-12-01 22:55:56 +0100

    model de interests

--- interests/models.py
+++ interests/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+def class InterestCategory()
+    name = models.CharField(max_length=512)
+
+
+def class Interests(models.Model):
+    name = models.CharField(max_length=512)
+    fb_id = models.CharField(max_length=64, unique=True)
+    category = models.ForeignKey(InterestCategory)
+
+
+def class UserInterests
+    user = models.ForeignKey(User)
+    interest = models.ForeignKey(Interest)
+    created = models.DateField()
+    
